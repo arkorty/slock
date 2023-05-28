b81ab890a01b89640a9eee40410ccacdf41b58cb
Arkaprabha Chakraborty <arkorty@gmail.com>
2023-05-28 21:39:10 +0530

    Modified config.def.h to use everforest colorscheme

--- config.def.h
+++ config.def.h
@@ -3,9 +3,10 @@ static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+    /* using everforest colorscheme */
+    [INIT]   = "#1E2326",   /* after initialization */
+    [INPUT]  = "#7FBBB3",  /* during input */
+    [FAILED] = "#E67E80", /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
